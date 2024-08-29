d193b09ea74bc07ce7b46d28d10d5ead63c8b901
marouane el moujahid <marwanmoujahid008@gmail.com>
2024-08-29 01:46:48 +0100

    working on list end-point

--- BackEnd/src/Controllers/ListControllers.ts
+++ BackEnd/src/Controllers/ListControllers.ts
@@ -1,20 +1,79 @@
-import { query } from "express";
 import db from "./../Config/PgConfig.js";
+import { TitleInputSchema } from "../Models/Schema.js";
 
-export const getListInfos = async (userID:string) => {
+export const getListInfos = async (userID: string, boardID: string) => {
     try {
-        /*
-        This query retrieves the names of all lists (listName) created by a specific user,identified by their userID. It joins the users,boards, and lists tables to filter the lists associated with the user's boards.
-        */
-        const query =`SELECT l."listName", l."listID"
-                    FROM "users" u
-                    JOIN "boards" b ON u."userID" = b."userID"
-                    JOIN "lists" l ON b."boardID" = l."boardID"
-                    WHERE u."userID" = $1;`;
-        const result = await db.query(query,[userID]);
+        const query = `SELECT list."listID", list."listName"
+                        FROM "lists" list 
+                        JOIN "boards" board ON list."boardID" = board."boardID"
+                        WHERE board."userID" = $1 AND board."boardID"=$2;`;
+        const result = await db.query(query, [userID, boardID]);
         return result.rows;
     } catch (error) {
-        console.error("Error fetching lists:", error.message);
-        return []; // Return an empty array in case of error
+        throw error;
     }
 };
+
+// Add list
+export async function addList(
+    listName: string,
+    boardID: string,
+    userID:string
+): Promise<void> {
+    try {
+        const query = `INSERT INTO "lists"("listName", "boardID")
+                        SELECT $1, "boardID"
+                        FROM "boards"
+                        WHERE "boardID" = $2 AND "userID" = $3;`;
+        const result = await db.query(query, [
+            TitleInputSchema.parse(listName),
+            boardID,
+            userID
+        ]);
+        return result;
+    } catch (err) {
+        throw new Error("Error removing lis");
+    }
+}
+
+// edit List
+export async function editList(
+    listID: string,
+    listName: string,
+    userID: string
+): Promise<void> {
+    try {
+        const query = `
+                    UPDATE "lists"
+                    SET "listName" = $1
+                    WHERE "listID" = $2 
+                    AND "boardID" IN (SELECT "boardID" FROM "boards" WHERE "userID" = $3);
+
+        `;
+        await db.query(query, [
+            TitleInputSchema.parse(listName),
+            listID,
+            userID,
+        ]);
+    } catch (err) {
+        throw new Error("Error updating list");
+    }
+}
+
+// Remove list
+export async function removeList(
+    listID: string,
+    userID: string
+): Promise<void> {
+    try {
+        const query = `
+        DELETE FROM "lists"
+        WHERE "listID" = $1 
+        AND "boardID" IN (SELECT "boardID" FROM "boards" WHERE "userID" = $2);
+    `;
+    
+        await db.query(query, [listID, userID]);
+    } catch (err) {
+        throw new Error("Error removing list");
+    }
+}
--- BackEnd/src/Routes/ListRoutes.ts
+++ BackEnd/src/Routes/ListRoutes.ts
@@ -1,15 +1,67 @@
 import express, { Request, Response } from "express";
-import { getListInfos } from "./../Controllers/ListControllers.js"; 
+import {
+    getListInfos,
+    addList,
+    removeList,
+    editList,
+} from "./../Controllers/ListControllers.js";
 
 const router = express.Router();
 
-router.get("/lists", async (req: Request, res: Response) => {
+router.get("/list/:boardID", async (req: Request, res: Response) => {
+    const userID = req.body.userID;
+    const { boardID } = req.params;
     try {
-        const result = await (req.body.userId);
+        const result = await getListInfos(userID, boardID);
         res.status(200).json(result);
     } catch (error) {
         console.error("Error fetching list information:", error);
         res.status(500).json([]);
+    }
+});
+
+// Add list
+router.post("/list", async (req: Request, res: Response) => {
+    const userID = req.body.userID;
+    const listName = req.body.listName;
+    const boardID = req.body.boardID;
+
+    if (!listName || !boardID)
+        return res.status(400).send({ message: "List name is required" });
+    try {
+        await addList(listName, boardID, userID);
+        return res.status(201).send({ message: "list added successfully" });
+    } catch (err) {
+        return res.status(500).send({ message: "Error adding list" });
+    }
+});
+
+// Edit list
+router.put("/list/:listID", async (req: Request, res: Response) => {
+    const { listID } = req.params;
+    const { listName, userID } = req.body;
+
+    if (!listName)
+        return res.status(400).send({ message: "Board name is required" });
+
+    try {
+        await editList(listID, listName, userID);
+        return res.status(200).send({ message: "List updated successfully" });
+    } catch (err) {
+        return res.status(500).send({ message: "Error updating List" });
+    }
+});
+
+// Delete list
+router.delete("/list/:listID", async (req: Request, res: Response) => {
+    const { listID } = req.params;
+    const userID = req.body.userID;
+    if (!listID) return res.status(400).send({ message: "List ID is required" });
+    try {
+        await removeList(listID, userID);
+        return res.status(200).send({ message: "list removed successfully" });
+    } catch (err) {
+        return res.status(500).send({ message: "Error removing list" });
     }
 });
 
